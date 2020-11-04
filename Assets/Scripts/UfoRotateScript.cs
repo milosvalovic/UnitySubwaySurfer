77c238571ce48b6ab56efb16828bdecffc177155
Miloš Valovič <valovic.milos1@gmail.com>
2020-11-04 14:36:41 +0100

    asset import + basic scene

---
+++ Assets/Scripts/UfoRotateScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UfoRotateScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.up, 80 * Time.deltaTime);
+    }
+}
