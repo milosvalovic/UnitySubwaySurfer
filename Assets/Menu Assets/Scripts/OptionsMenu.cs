7366732852b5b1bcc57c2c253bf090b080386952
DuS4NN <34943373+DuS4NN@users.noreply.github.com>
2020-12-01 12:14:25 +0100

    add graphic settings

--- Assets/Menu Assets/Scripts/OptionsMenu.cs
+++ Assets/Menu Assets/Scripts/OptionsMenu.cs
@@ -16,4 +16,19 @@ public class OptionsMenu : MonoBehaviour
     {
         audioMixer.SetFloat("SoundEffects", volume);
     }
+    
+    public void setQuality (int qualityIndex)
+    {
+        if(qualityIndex == 0)
+        {
+            QualitySettings.SetQualityLevel(2);
+        }else if(qualityIndex == 2) 
+        {
+            QualitySettings.SetQualityLevel(0);
+        }else 
+        {
+            QualitySettings.SetQualityLevel(1);
+        }
+        
+    }
 }
