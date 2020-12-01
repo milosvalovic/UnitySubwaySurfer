8dd717829af281873721d815b4e10d79da1c71f9
DuS4NN <34943373+DuS4NN@users.noreply.github.com>
2020-12-01 11:19:54 +0100

    add sound controls

---
+++ Assets/Menu Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetMusicVolume (float volume)
+    {
+        audioMixer.SetFloat("Music", volume);
+    }
+
+    public void SetSoundEffectVolume (float volume)
+    {
+        audioMixer.SetFloat("SoundEffects", volume);
+    }
+}
